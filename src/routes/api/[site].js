1199e038c551f27eedef7a8c7504b5c55f40fcf6
mateomorris <accounts@mateomorris.com>
2021-10-04 11:26:17 +0200

    fix save endpoint

--- src/routes/api/[site].js
+++ src/routes/api/[site].js
@@ -1,12 +1,10 @@
 import * as actions from '../../actions'
 import * as supabaseDB from '../../supabase/db'
-import * as supabaseStorage from '../../supabase/storage'
 
-export async function post(request) {
-  const { site:siteID, token } = request.params
+export async function post({ body }) {
+  const { site, token } = body
   const storedToken = await supabaseDB.config.get('server-token')
   if (storedToken && token === storedToken) {
-    const { site } = request.body
     await actions.sites.save(site)
     return {
       body: 'ok'
