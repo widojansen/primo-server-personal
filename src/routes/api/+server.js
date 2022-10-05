00c2f732cefb32a32ce8a356126448988b2b06da
Mateo Morris <accounts@mateomorris.com>
2022-10-04 21:50:22 -0400

    try without status and using previous options

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -8,12 +8,11 @@ export async function GET(event) {
 
 export async function OPTIONS() {
   return new Response(JSON.stringify({
-    status: 201,
-    Headers: {
-      'Access-Control-Allow-Origin': '*', // only allow requests from Electron
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Allow': '*'
-    }
+    // status: 201,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		},
   }))
 }
