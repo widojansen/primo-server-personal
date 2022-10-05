6711f3f2890cc11512a9df0a55504527e2678bba
Mateo Morris <accounts@mateomorris.com>
2022-10-05 01:02:04 -0400

    move response headers

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -3,12 +3,13 @@ import { authorizeRequest } from './_auth'
 export async function GET(event) {
   return await authorizeRequest(event, async () => {
     return new Response(JSON.stringify({ 
+      body: {success: true} 
+    }), {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
-      body: {success: true} 
-    }))
+    })
   })
 }
