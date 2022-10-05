6dfb7f9c2b0f6c719d7870f03850789ba418d005
Mateo Morris <accounts@mateomorris.com>
2022-10-04 22:32:53 -0400

    attach headers to GET

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -2,7 +2,14 @@ import { authorizeRequest } from './_auth'
 
 export async function GET(event) {
   return await authorizeRequest(event, async () => {
-    return new Response(JSON.stringify({ body: {success: true} }))
+    return new Response(JSON.stringify({ 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+      body: {success: true} 
+    }))
   })
 }
 
