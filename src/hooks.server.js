4de92bb085fcc4189d0b90f8485837aa5f1facdb
Mateo Morris <accounts@mateomorris.com>
2022-10-04 23:33:17 -0400

    add headers to handler

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,5 +2,12 @@ import Blob from 'cross-blob';
 
 export const handle = async ({event, resolve}) => {
   if (event.request.method !== "OPTIONS") return await resolve(event)
-  return new Response(new Blob(), {status: 200})
+  return new Response(new Blob(), {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    }
+  })
 }
