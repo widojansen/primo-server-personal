41cd71f3a62f2a7eaad19945654af3bee7161c9f
Mateo Morris <accounts@mateomorris.com>
2022-10-04 23:24:18 -0400

    add hook

---
+++ src/hooks.server.js
@@ -0,0 +1,6 @@
+import Blob from 'cross-blob';
+
+export const handle = async ({event, resolve}) => {
+  if (event.request.method !== "OPTIONS") return await resolve(event)
+  return new Response(new Blob(), {status: 200})
+}
--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -3,23 +3,7 @@ import { authorizeRequest } from './_auth'
 export async function GET(event) {
   return await authorizeRequest(event, async () => {
     return new Response(JSON.stringify({ 
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
       body: {success: true} 
     }))
   })
 }
-
-// export async function OPTIONS() {
-//   return new Response(JSON.stringify({
-//     status: 201,
-// 		headers: {
-// 			'Access-Control-Allow-Origin': '*',
-// 			'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-// 			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-// 		},
-//   }))
-// }
