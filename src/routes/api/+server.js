9af8e6f1c44ff61ab47c1b6f1b0056828f756d6b
Mateo Morris <accounts@mateomorris.com>
2022-10-04 22:28:42 -0400

    remove options

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -6,13 +6,13 @@ export async function GET(event) {
   })
 }
 
-export async function OPTIONS() {
-  return new Response(JSON.stringify({
-    status: 201,
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-		},
-  }))
-}
+// export async function OPTIONS() {
+//   return new Response(JSON.stringify({
+//     status: 201,
+// 		headers: {
+// 			'Access-Control-Allow-Origin': '*',
+// 			'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+// 			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+// 		},
+//   }))
+// }
