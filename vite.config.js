fcee7df84937e7443d5ed6af17071f00a1d9690e
Mateo Morris <accounts@mateomorris.com>
2022-10-04 23:08:48 -0400

    add headers to vite

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ const config = {
       allow: ['..', 'node_modules/@fontsource/fira-code']
     },
     proxy: {},
-    port: 5174
+    port: 5174,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    }
   },
   define: {
     '__SERVER_VERSION__': JSON.stringify(process.env.npm_package_version),
